ce1ad4874abf30b9ea70ce39812e43156d7e444c
Karthik <appannagarikarthik.123@gmail.com>
2021-10-11 12:31:34 +0530

    initial commit

---
+++ stcok_bot.py
@@ -0,0 +1,20 @@
+import discord
+
+from discord.ext import commands
+
+
+client = commands.Bot(command_prefix = '^')
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('logged in as: %s' % client.user.name)
+        print('ID is:', client.user.id)
+
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+
+client.run('ODk3MDA0NDk2NDQ4ODExMDc5.YWPWiQ.F3JhILhRcKy0I3Rx-4Be6Lpf5Bs')
