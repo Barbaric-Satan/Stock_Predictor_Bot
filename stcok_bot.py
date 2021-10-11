8ee787263061f24d36c5959ae721c3a8ae9d25cf
Karthik <appannagarikarthik.123@gmail.com>
2021-10-11 12:35:20 +0530

    Function added by karthik

--- stcok_bot.py
+++ stcok_bot.py
@@ -15,6 +15,9 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
+    async def karthik(self):
+        print("This line is added by karthik")
+
 client = MyClient()
 
 client.run('ODk3MDA0NDk2NDQ4ODExMDc5.YWPWiQ.F3JhILhRcKy0I3Rx-4Be6Lpf5Bs')
